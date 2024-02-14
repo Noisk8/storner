5c7d4d26833eee5b565a6cf1d2024eb5914125bf
Noisk8 <juansk8jaramillo@gmail.com>
2024-02-14 02:46:17 -0500

    13

--- src/componentes/home/Description/Description.tsx
+++ src/componentes/home/Description/Description.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image"
+import styles from './Description.module.sass';
 
-export const Description = () =>{
-return(
-
-<section>
-<h1>Vamo que vamos 🎱 </h1>
-<img src="https://raw.githubusercontent.com/platzi/nextjs-14/class/statics/public/images/description.jpeg" alt="q verguero"></img>
-<p>Hola se que esto no es es así</p>
-
-</section>
-)
-
+export const Description = () => {
+  return (
+    <section className={styles.Description}>
+     <img src="https://raw.githubusercontent.com/platzi/nextjs-14/class/statics/public/images/description.jpeg" alt="q verguero"></img>
+      <div className={styles.Description__text}>
+        <h2>Bring the future today</h2>
+        <p>Future World: Your Gateway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
+      </div>
+    </section>
+  )
 }
+
